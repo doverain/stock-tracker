ab415d123d32c0d5fc45a6e896ac3471a4fdb57f
doverain <lpilipen@yahoo.com>
2021-07-14 12:44:22 -0700

    Lots. Incl add gems and forms. Up until tables.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,21 @@
 class HomeController < ApplicationController
   def index
-  end
+    @api = StockQuote::Stock.new(api_key: 
+     'pk_6730195fe96d4172b0001facd3fc8dbc')
+
+    if params[:ticker] == ""
+        @nothing_msg = "Please Enter a Symbol"
+    elsif params[:ticker]
+      # check the stock symbol and get info:
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      if !@stock
+          @invalid_symbol_msg = "That stock doesn't exist. Please enter a valid symbol."
+      end
+
+    end # if params[:ticker] == null
+  end #def
+
   def about
   end
-end
+
+end #class
